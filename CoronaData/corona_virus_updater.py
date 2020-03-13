20998cb08336b62608b58400a6fa0739d5b2b961
sergenp <sergen12343@gmail.com>
2020-03-13 21:17:05 +0300

    added sending news to a specific channel

--- CoronaData/corona_virus_updater.py
+++ CoronaData/corona_virus_updater.py
@@ -11,11 +11,12 @@ except FileExistsError:
     pass
 
 
-def update_data():
-    header = {
+header = {
   "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
   "X-Requested-With": "XMLHttpRequest"
 }
+
+def update_data():
     r = session.get('https://www.worldometers.info/coronavirus/', headers=header)
 
     title = ["Total Cases","Total Deaths","Total Recovered"]
@@ -28,26 +29,29 @@ def update_data():
     with open('CoronaData/total_inf.json', 'w') as outfile:
         json.dump(total_inf, outfile)
 
-
     tables = pd.read_html(r.text)
-    print(tables)
     cv19_table = tables[0]
     cv19_table = cv19_table.drop("Tot Cases/1M pop", axis=1).rename(columns = {"Country,Other" : "Country", "Serious,Critical" : "Serious"})[:-1]
     cv19_table.to_json("CoronaData/data.json", orient='records')
 
+    
+
 def get_corona_news():
-    r = session.get('https://www.worldometers.info/coronavirus/')
+    r = session.get('https://www.worldometers.info/coronavirus/', headers=header)
     today = r.html.find("#innercontent > h4", first=True).text.replace("(GMT):", "").replace(" ", "")
     news = [x.text.replace("[source]", "") for x in r.html.find("#innercontent > ul > li")]
-    info = {today : news}
+    info = {0 : news}
 
     # lets check if there any new news
     if os.path.exists(f'CoronaData/news/{today}.json'):
         old_news = json.load(open(f'CoronaData/news/{today}.json', 'r'))
         if len(old_news[today]) == len(info[today]):
             return False
+        else:
+            return list(set(old_news[today]) - set(info[today]))
+    else:    
+        with open(f'CoronaData/news/{today}.json', 'w') as outfile:
+            json.dump(info, outfile)
+        return news
     
-    with open(f'CoronaData/news/{today}.json', 'w') as outfile:
-        json.dump(info, outfile)
-    return True
 
--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-  
+# -*- coding: utf-8 -*-
 from discord.ext import tasks, commands
-from CoronaData.corona_virus_updater import update_data
+from CoronaData.corona_virus_updater import update_data, get_corona_news
+import datetime
 
 bot = commands.Bot(command_prefix="h!")
 
@@ -14,16 +15,30 @@ for extension in startup_extensions:
         print('Failed to load extension {}\n{}'.format(extension, exc))
 
  # update corona virus data every x mins
+
+
 @tasks.loop(hours=0.1)
 async def corona_update_task():
     print("Updating coronavirus data")
     update_data()
     print("Updated coronavirus data")
 
+
+@tasks.loop(hours=0.1)
+async def corona_update_news_task():
+    news = get_corona_news()
+    channel = bot.get_channel(688045137162666051)
+    if news:
+        for k in news:
+            await channel.send(k)
+
+
 @bot.event
 async def on_ready():
     print(f"Connected!\nName: {bot.user.name}\nId: {bot.user.id}\n")
     corona_update_task.start()
+    corona_update_news_task.start()
+
 
 try:
     import bot_token
