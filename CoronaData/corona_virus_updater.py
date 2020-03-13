699cf34cab335432680cfa6eb3acfbb0980db807
sergenp <sergen12343@gmail.com>
2020-03-13 19:59:03 +0300

    added scraping for corona news

--- CoronaData/corona_virus_updater.py
+++ CoronaData/corona_virus_updater.py
@@ -3,8 +3,9 @@ import pandas as pd
 import os
 import json
 
+session = HTMLSession()
+
 def update_data():
-    session = HTMLSession()
     r = session.get('https://www.worldometers.info/coronavirus/')
 
     title = ["Total Cases","Total Deaths","Total Recovered"]
@@ -21,3 +22,20 @@ def update_data():
     cv19_table = tables[0]
     cv19_table = cv19_table.drop("Tot Cases/1M pop", axis=1).rename(columns = {"Country,Other" : "Country", "Serious,Critical" : "Serious"})[:-1]
     cv19_table.to_json("CoronaData/data.json", orient='records')
+
+def get_corona_news():
+    r = session.get('https://www.worldometers.info/coronavirus/')
+    today = r.html.find("#innercontent > h4", first=True).text.replace("(GMT):", "").replace(" ", "")
+    news = [x.text.replace("[source]", "") for x in r.html.find("#innercontent > ul > li")]
+    info = {today : news}
+
+    # lets check if there any new news
+    if os.path.exists(f'CoronaData/news/{today}.json'):
+        old_news = json.load(open(f'CoronaData/news/{today}.json', 'r'))
+        if len(old_news[today]) == len(info[today]):
+            return False
+    
+    with open(f'CoronaData/news/{today}.json', 'w') as outfile:
+        json.dump(info, outfile)
+    return True
+
