426475fd338276a52b119d14e872c0cd0cc6dcfb
sergenp <sergen12343@gmail.com>
2020-11-19 10:16:53 +0300

    Answers will be registered as 1 if users add any other reaction

--- big5_test.py
+++ big5_test.py
@@ -13,13 +13,13 @@ class Big5TestCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
     
-    @commands.command(pass_context=True,aliases=["big5test", "b5test", "ptest"], description="A Big 5 Personality test")
+    @commands.command(pass_context=True,aliases=["big5test", "ptest"], description="A Big 5 Personality test")
     async def b5test(self, ctx):
         tests[ctx.author.id] = Big5Test(ctx.author.id)
         await ctx.send("Successfully created a Big5 test for you, please answer the questions accordingly, note that your Big5 will be saved at the end of the test.")
         return await self.test_loop(ctx, ctx.author.id)
 
-    @commands.command(pass_context=True, aliases=["b5result", "myb5", "big5"], description="A Big 5 Personality test")
+    @commands.command(pass_context=True, aliases=["b5result"], description="A Big 5 Personality test")
     async def myb5(self, ctx):
         res = MongoDB.get_big5_results(ctx.author.id)
         if res:
@@ -35,7 +35,7 @@ class Big5TestCog(commands.Cog):
         test = tests[test_id]
         curr_q = test.get_current_question()        
         em = Embed(description=curr_q.body, title=f"Question #{curr_q.Id} for {ctx.author.name}")
-        em.set_footer(text=f"1=Disagree, 3=Neutral and 5=Agree. Currently at {curr_q.Id}/50. You have a minute to answer each question")
+        em.set_footer(text=f"1=Disagree, 3=Neutral and 5=Agree. Currently at {curr_q.Id}/50. You have a minute to answer each question.\n\nNote that if you add any other reaction, your answer will be registered as 1")
 
         if not msg:
             msg = await ctx.send(embed=em)
@@ -49,7 +49,7 @@ class Big5TestCog(commands.Cog):
 
         try:
             reaction, _ = await self.bot.wait_for('reaction_add', timeout=60.0, check=check)
-            test.answer_question(reactions_map[reaction.emoji])
+            test.answer_question(reactions_map.get(reaction.emoji, 1))
         
         except asyncio.TimeoutError:
             await ctx.send("You haven't answered the question in time, deleting the test")
